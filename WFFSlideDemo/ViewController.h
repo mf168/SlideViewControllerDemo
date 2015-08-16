a943007b51dad135c71a44034bead6449239955d
ios_wf <1032351062@qq.com>
2015-08-16 13:13:13 +0800

    Initial Commit

---
+++ WFFSlideDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WFFSlideDemo
+//
+//  Created by wff on 15/8/16.
+//  Copyright (c) 2015年 hct. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ WFFSlideDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WFFSlideDemo
+//
+//  Created by wff on 15/8/16.
+//  Copyright (c) 2015年 hct. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
